d48f2d5e38594b5bf4ade0e0a56160c9bbb7a4fa
Ryan Paglinawan <rymotion1@me.com>
2015-04-14 17:41:11 -0700

    Starting up the update stuff
    more update stuff

---
+++ Howl/Howl/Info.h
@@ -0,0 +1,13 @@
+//
+//  Info.h
+//  Howl
+//
+//  Created by Ryan Paglinawan on 4/14/15.
+//  Copyright (c) 2015 Ryan Paglinawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Info : UIScrollView
+
+@end
---
+++ Howl/Howl/Info.m
@@ -0,0 +1,21 @@
+//
+//  Info.m
+//  Howl
+//
+//  Created by Ryan Paglinawan on 4/14/15.
+//  Copyright (c) 2015 Ryan Paglinawan. All rights reserved.
+//
+
+#import "Info.h"
+
+@implementation Info
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
